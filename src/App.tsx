c5bdac65b10c4afff358c010dc51a438e8274f8e
Jabberino <darlito_guamos@dlsu.edu.ph>
2024-12-02 22:30:00 +0800

    Added email at bottom of page. Downloaded file name now include term and year.

--- src/App.tsx
+++ src/App.tsx
@@ -7,10 +7,9 @@ import {schedule} from "./interfaces.ts";
 
 export default function App() {
     const [acceptedFile, setAcceptedFile] = useState<null | File>(null)
+    const [termDetails, setTermDetails] = useState<{ay: number, term: number} | undefined>()
     const [downloadLink, setDownloadLink] = useState("")
     const [error, setError] = useState('');
-
-
 
     const onDrop = useCallback(async (acceptedFiles: File[]) => {
         let newError = "";
@@ -22,46 +21,41 @@ export default function App() {
         } else if (acceptedFiles[0].type !== "application/pdf") {
             newError = "EAF must be in PDF form";
         } else { // is a single PDF
-            let res;
             try {
-                res = await extractYearTermFromPDF(URL.createObjectURL(acceptedFiles[0]));
+                const res = await extractYearTermFromPDF(URL.createObjectURL(acceptedFiles[0]));
                 if (!res) {
                     newError = "Something unexpected went wrong";
                 }
-
-                // @ts-expect-error above conditional catches null
-                const res2 = await termIsSupported(res);
-
-                if(!res2){
-                    // @ts-expect-error conditional catches null
-                    newError = `AY ${res.ay}, Term ${res.term} is not supported`;
-                }
                 else{
-                    newFile = acceptedFiles[0];
-                    // for now, this will just put a download button
-                    let schedule: schedule = {
-                        classes: undefined,
-                        // @ts-expect-error conditional catches null
-                        ay: res.ay,
-                        // @ts-expect-error conditional catches null
-                        term: res.term,
-                        start: new Date(res2.start),
-                        end: new Date(res2.end),
-                    }
+                    setTermDetails(res);
+                    const res2 = await termIsSupported(res);
 
-                    schedule = await extractClassesFromPDF(URL.createObjectURL(acceptedFiles[0]),schedule);
-                    try{
-                        newDownloadLink = await createICS(schedule);
-                    }catch(error){
-                        if(error instanceof Error){
-                            newError = error.message;
+                    if(!res2){
+                        newError = `AY ${res.ay}, Term ${res.term} is not supported`;
+                    }
+                    else{
+                        newFile = acceptedFiles[0];
+                        // for now, this will just put a download button
+                        let schedule: schedule = {
+                            classes: undefined,
+                            ay: res.ay,
+                            term: res.term,
+                            start: new Date(res2.start),
+                            end: new Date(res2.end),
                         }
-                        else{
-                            console.error("This shouldn't happen");
+                        schedule = await extractClassesFromPDF(URL.createObjectURL(acceptedFiles[0]),schedule);
+                        try{
+                            newDownloadLink = await createICS(schedule);
+                        }catch(error){
+                            if(error instanceof Error){
+                                newError = error.message;
+                            }
+                            else{
+                                console.error("This shouldn't happen");
+                            }
                         }
                     }
                 }
-
             } catch (error) {
                 if (error instanceof Error) {
                     newError = error.message;
@@ -69,8 +63,6 @@ export default function App() {
                     console.error("This shouldn't happen");
                 }
             }
-
-
         }
         if(newError !== error){
             setError(newError);
@@ -90,18 +82,20 @@ export default function App() {
     } = useDropzone({onDrop})
 
 
-    console.log(downloadLink);
-
     return (
         <div className="flex flex-col">
-            <Upload  acceptedFile={acceptedFile} error={error} getInputProps={getInputProps} getRootProps={getRootProps}/>
-            {acceptedFile && downloadLink &&
+            <Upload acceptedFile={acceptedFile} error={error} getInputProps={getInputProps}
+                    getRootProps={getRootProps}/>
+            {acceptedFile && downloadLink && termDetails &&
                 // <div className="flex justify-center w-1/2 min-h-96 self-center border rounded-md drop-shadow-lg bg-white mb-24">
                 //     {}
                 // </div>
-                <a href={downloadLink} download={`Schedule.ics`} className="flex flex-grow border self-center bg-darkgreen rounded-lg p-2 sm:p-3 px-4 sm:px-7 text-white text-sm sm:text-xl text-center drop-shadow-lg">Download</a>
+                <a href={downloadLink} download={`Schedule-AY${termDetails.ay}-T${termDetails.term}.ics`}
+                   className="flex flex-grow border self-center bg-darkgreen rounded-lg p-2 sm:p-3 px-4 sm:px-7 text-white text-sm sm:text-xl text-center drop-shadow-lg">Download</a>
             }
-
+            <div className="fixed bottom-0 left-0 right-0 bg-gray-900 text-white text-center p-2"> For any bugs (or internships 🙂),
+                email me: <a href="mailto:darlito_guamos@dlsu.edu.ph" className="underline">darlito_guamos@dlsu.edu.ph</a>
+            </div>
         </div>
 
     )
